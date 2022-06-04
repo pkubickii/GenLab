bbed57a7fdd5986085630a3521478e1d518bdef9
przemo <przemo@pop-os.localdomain>
2022-06-05 01:28:38 +0200

    feat: particle swarm + vicinity as %

--- pages/ps.py
+++ pages/ps.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import numpy as np
 import pandas as pd
 import utils.compute as cmp
+import os
 
 
 def get_v(x, v, bl, bg, c1, c2, c3):
@@ -11,55 +12,82 @@ def get_v(x, v, bl, bg, c1, c2, c3):
     return c1 * r[0] * float(v) + c2 * r[1] * (float(bl) - float(x)) + c3 * r[2] * (float(bg) - float(x))
 
 
-def particle_swarm(a, b, n, d, time_t, c1, c2, c3):
+def get_bg_for(p_id: int, particles: pd.DataFrame, vicinity: float):
+    df = particles.copy()
+    pdf = df.iloc[[p_id]]
+    dist = []
+    p = float(df["particle"][p_id])
+    df = df.drop(index=p_id)
+    for x in df["particle"]:
+        dist.append(abs(p - float(x)))
+    df.insert(loc=4, column="dist", value=dist)
+    df = df.sort_values("dist")
+    how_many = round(vicinity / 100 * df.shape[0])
+    nbhood = pd.concat([pdf, df.iloc[:how_many]])
+    nbhood = nbhood.sort_values("bg_fxs", ascending=False)
+    nbhood = nbhood.reset_index()
+    return nbhood["bg"][0]
+
+
+def particle_swarm(a, b, n, d, time_t, c1, c2, c3, vicinity):
+    dot_places = cmp.compute_precision(d)
     particles = cmp.add_precision(cmp.generate_population(a, b, n), d)
     vs = cmp.add_precision(cmp.generate_population(a, b, n), d)
     best_locals = list(particles)
-    bounds = ['-4.000', '12.000']
-
+    best_globals = list(particles)
+    bounds = [f'{a:.{dot_places}f}', f'{b:.{dot_places}f}']
+    p_fxs = [cmp.compute_fx(float(x)) for x in particles]
+    bl_fxs = [cmp.compute_fx(float(x)) for x in best_locals]
+    bg_fxs = [cmp.compute_fx(float(x)) for x in best_globals]
     df = pd.DataFrame({
         "particle": particles,
-        "fp": [cmp.compute_fx(float(x)) for x in particles],
+        "p_fxs": p_fxs,
+        "bl": best_locals,
+        "bl_fxs": bl_fxs,
+        "bg": best_globals,
+        "bg_fxs": bg_fxs,
     })
-
-    fxs_sorted = df.sort_values("fp", ascending=False)
-    best_global = particles[fxs_sorted.index[0]]
     results = [df]
 
     for t in range(time_t):
-        p_fxs = [cmp.compute_fx(float(x)) for x in particles]
-        bl_fxs = [cmp.compute_fx(float(x)) for x in best_locals]
-        bg_fx = cmp.compute_fx(float(best_global))
+        best_globals = [get_bg_for(i, df, vicinity) for i in range(n)]
+        bg_fxs = [cmp.compute_fx(float(x)) for x in best_globals]
         new_particles = []
-        new_vs = np.zeros(len(particles)).tolist()
-        for i in range(len(particles)):
+        new_vs = np.zeros(n).tolist()
+        for i in range(n):
             if p_fxs[i] > bl_fxs[i]:
                 best_locals[i] = particles[i]
-            elif p_fxs[i] > bg_fx:
-                best_global = particles[i]
-        for i in range(len(particles)):
+            elif p_fxs[i] > bg_fxs[i]:
+                best_globals[i] = particles[i]
+        for i in range(n):
             new_vs[i] = get_v(
                 particles[i],
                 vs[i],
                 best_locals[i],
-                best_global,
+                best_globals[i],
                 c1, c2, c3)
             new_particle = float(particles[i]) + new_vs[i]
-            if np.round(new_particle, 3) < float(bounds[0]):
+            if round(new_particle, dot_places) < round(float(bounds[0]), dot_places):
                 new_particle = float(bounds[0])
-            elif np.round(new_particle, 3) > float(bounds[1]):
+            elif round(new_particle, dot_places) > round(float(bounds[1]), dot_places):
                 new_particle = float(bounds[1])
             new_particles.append(new_particle)
         new_particles = cmp.add_precision(new_particles, d)
         particles = list(new_particles)
         vs = list(new_vs)
-        results.append(pd.DataFrame({
+        p_fxs = [cmp.compute_fx(float(x)) for x in particles]
+        bl_fxs = [cmp.compute_fx(float(x)) for x in best_locals]
+        bg_fxs = [cmp.compute_fx(float(x)) for x in best_globals]
+        df = pd.DataFrame({
             "particle": particles,
-            "fp": p_fxs,
-        }))
+            "p_fxs": p_fxs,
+            "bl": best_locals,
+            "bl_fxs": bl_fxs,
+            "bg": best_globals,
+            "bg_fxs": bg_fxs,
+        })
+        results.append(df)
     return results
-
-
 
 
 form = dbc.Form(
@@ -104,7 +132,7 @@ form = dbc.Form(
                         dbc.InputGroup([
                             dbc.InputGroupText("N:"),
                             dbc.Input(id='n_value', type='number', min=1,
-                                      max=5000, placeholder='N', value=50),
+                                      max=5000, placeholder='N', value=100),
                         ], className='me-3'),
                         ], width=2),
                 dbc.Col([
@@ -114,11 +142,44 @@ form = dbc.Form(
                                       max=5000, placeholder='T', value=100),
                         ], className='me-3'),
                         ], width=2),
+            ]),
+        dbc.Row([
                 dbc.Col([
+                    dbc.InputGroup([
+                        dbc.InputGroupText("c1:"),
+                        dbc.Input(id='c1_value', type='number', min=0, max=3,
+                                  placeholder='c1', value=0.8),
+                    ], className='me-3'),
+                ], width=2),
+                dbc.Col([
+                    dbc.InputGroup([
+                        dbc.InputGroupText("c2:"),
+                        dbc.Input(id='c2_value', type='number', min=0, max=3,
+                                  placeholder='c2', value=1.0),
+                    ], className='me-3'),
+                ], width=2),
+                dbc.Col([
+                    dbc.InputGroup([
+                        dbc.InputGroupText("c3:"),
+                        dbc.Input(id='c3_value', type='number', min=0, max=3,
+                                  placeholder='c3', value=1.2),
+                    ], className='me-3'),
+                ], width=2),
+                dbc.Col([
+                    dbc.InputGroup([
+                        dbc.InputGroupText("v:"),
+                        dbc.Input(id='v_value', type='number', min=1, max=100,
+                                  placeholder='vicinity', value=50),
+                        dbc.InputGroupText("%"),
+                    ], className='me-3'),
+                ], width=2),
+                dbc.Col([
+                    html.Div([
                         dbc.Button("Uruchom PC", outline=True, color='info',
                                    size='lg', id='submit_button', n_clicks=0),
-                        ], width=2)
-            ])
+                    ], style={'textAlign': 'center'}),
+                ], width=2),
+                ], className='mt-2'),
     ])
 
 
@@ -191,9 +252,13 @@ def get_size(fxs):
            State('d_value', 'value'),
            State('n_value', 'value'),
            State('t_value', 'value'),
+           State('c1_value', 'value'),
+           State('c2_value', 'value'),
+           State('c3_value', 'value'),
+           State('v_value', 'value'),
            prevent_initial_call=True)
-def get_ps(n_clicks, input_a, input_b, input_d, input_n, input_t):
-    if None in [input_a, input_b, input_d, input_n, input_t]:
+def get_ps(n_clicks, input_a, input_b, input_d, input_n, input_t, input_c1, input_c2, input_c3, input_v):
+    if None in [input_a, input_b, input_d, input_n, input_t, input_c1, input_c2, input_c3, input_v]:
         return no_update, no_update, html.Div(
             "Pola wypełniamy wartościami numerycznymi.", style={'color': 'red'})
     elif int(np.ma.round(input_a)) == int(np.ma.round(input_b)):
@@ -212,28 +277,35 @@ def get_ps(n_clicks, input_a, input_b, input_d, input_n, input_t):
         b = int(np.ma.round(input_b))
 
     d = float(input_d)
+    dot_places = cmp.compute_precision(d)
     n = int(input_n)
     time_t = int(input_t)
-    c1 = 0.8
-    c2 = 1.0
-    c3 = 1.2
-    df_list = particle_swarm(a, b, n, d, time_t, c1, c2, c3)
+    c1 = round(float(input_c1), 1)
+    c2 = round(float(input_c2), 1)
+    c3 = round(float(input_c3), 1)
+    vicinity = round(float(input_v))
+    df_list = particle_swarm(a, b, n, d, time_t, c1, c2, c3, vicinity)
     zeros = np.zeros(n * time_t)
     times = np.arange(time_t)
     df = pd.DataFrame()
     df = pd.concat(df_list, keys=times)
     df = df.reset_index()
-    df.columns = ["time_t", "p_id", "particle", "fp"]
+    df.columns = ["time_t", "p_id", "particle",
+                  "p_fxs", "bl", "bl_fxs", "bg", "bg_fxs"]
     df.insert(loc=0, column="zeros", value=zeros)
-    df['particle'] = df['particle'].astype(float)
+    size = get_size(df["p_fxs"])
+    df.insert(loc=9, column="size", value=size)
 
-    size = get_size(df["fp"]) 
-    df.insert(loc=5, column="size", value=size)
+    path = './results'
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
     df.to_csv("./results/psresult.csv")
+    df['particle'] = df['particle'].astype(float)
     ps_fig = px.scatter(
         df,
         x="particle",
-        y="fp",
+        y="zeros",
         color="p_id",
         size="size",
         size_max=20,
@@ -241,6 +313,16 @@ def get_ps(n_clicks, input_a, input_b, input_d, input_n, input_t):
         animation_group="p_id",
         range_x=[-4, 12],
         range_y=[-2, 2],
+        hover_data={
+            "p_fxs": f':.{dot_places}f',
+            "bl": True,
+            "bl_fxs": f':.{dot_places}f',
+            "bg": True,
+            "bg_fxs": f':.{dot_places}f',
+            "zeros": False,
+            "time_t": False,
+            "size": False,
+        }
     )
     ps_fig.update_layout({'xaxis.autorange': True, 'yaxis.autorange': True})
     return no_update, dcc.Graph("ps_result_graph", figure=ps_fig, config={
